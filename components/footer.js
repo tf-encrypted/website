051bda5275d9bfa0b4f38b9a5afeadcf7a795738
Ben DeCoste <bendecoste@gmail.com>
2019-06-24 14:57:00 -0300

    added footer

--- components/footer.js
+++ components/footer.js
@@ -8,6 +8,12 @@ class Footer extends React.Component {
         return (
             <div className='footer'>
                 <img src={ tfeImg }/>
+                <div className='links'>
+                    <a href='https://github.com/tf-encrypted/tf-encrypted'>View on Github</a>
+                    <a href='https://join.slack.com/t/tf-encrypted/shared_invite/enQtNjI5NjY5NTc0NjczLTFkYTRjYWQ0ZWVlZjVmZTVhODNiYTA2ZTdlNWRkMWE4MTI3ZGFjMWUwZDhhYTk1NjJkZTRiZjBhMzMyMjNlZmQ'>Join our Slack Community</a>
+                    <a href='https://arxiv.org/abs/1810.08130'>Read our Whitepaper</a>
+                    <a href='https://github.com/tf-encrypted/tf-encrypted/tree/master/examples'>Examples</a>
+                </div>
             </div>
         )
     }
