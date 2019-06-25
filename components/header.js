5340641f863108b9e9c8b51d4cd9c04e83c4027f
Ben DeCoste <bendecoste@gmail.com>
2019-06-25 11:44:49 -0300

    mobile menu

--- components/header.js
+++ components/header.js
@@ -3,32 +3,36 @@ import Head from 'next/head'
 
 import '../style/app.scss'
 import img from '../img/tfe.png'
+import * as logo from '../img/tfe-logo.png'
 import * as github from '../img/github.png'
-
-let links = (
-    <div className='links'>
-            <Head>
-                <script async defer src='https://buttons.github.io/buttons.js'></script>
-            </Head>
-            <a className='community' href='#community'>Community</a>
-            <a href='https://tf-encrypted.readthedocs.io/en/latest/'>Documentation</a>
-            <a className='cta-link' href='https://github.com/tf-encrypted/tf-encrypted'>
-                <img src={github}/>
-            </a>
-            <a className='github-button' href='https://github.com/tf-encrypted/tf-encrypted' data-icon='octicon-star' data-size='large' data-show-count='true' aria-label='Star ntkme/github-buttons on GitHub'>Star</a>
-    </div>
-)
 
 class Overflow extends React.Component {
 
     render () {
         return (
             <div className='hamburger-links'>
-                <div className='close' onClick={this.props.closeClicked.bind(this.props)}>
+                <div className='mobile-close-wrapper' onClick={this.props.closeClicked.bind(this.props)}>
+                    <div className='mobile-close'></div>
                 </div>
                 <div className='background'>
                 </div>
-                { links }
+                <div className='links'>
+                    <div className='logo'>
+                        <img src={logo}/>
+                    </div>
+
+                    <a className='community' onClick={this.props.closeClicked.bind(this.props)} href='#community'>Community</a>
+                    <a href='https://tf-encrypted.readthedocs.io/en/latest/'>Documentation</a>
+
+                    <div className='footer'>
+                        <div className='github-wrapper'>
+                            <a className='cta-link' href='https://github.com/tf-encrypted/tf-encrypted'>
+                                <img src={github}/>
+                            </a>
+                            <a className='github-button' href='https://github.com/tf-encrypted/tf-encrypted' data-icon='octicon-star' data-size='large' data-show-count='true' aria-label='Star ntkme/github-buttons on GitHub'>Star</a>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
@@ -42,14 +46,24 @@ class Header extends React.Component {
     render () {
         return (
             <div className='header' id='top-header'>
+                <Head>
+                    <script async defer src='https://buttons.github.io/buttons.js'></script>
+                </Head>
                 <div className='logo'>
                     <div className='logo-wrapper'>
                         <img className='logo-icon' src={img}/>
                     </div>
                 </div>
 
-                { links }
+                <div className='links'>
+                    <a className='community' href='#community'>Community</a>
+                    <a href='https://tf-encrypted.readthedocs.io/en/latest/'>Documentation</a>
+                    <a className='cta-link' href='https://github.com/tf-encrypted/tf-encrypted'>
+                        <img src={github}/>
+                    </a>
+                    <a className='github-button' href='https://github.com/tf-encrypted/tf-encrypted' data-icon='octicon-star' data-size='large' data-show-count='true' aria-label='Star ntkme/github-buttons on GitHub'>Star</a>
 
+                </div>
                 <div className='hamburger' onClick={this.props.hamburgerClicked.bind(this.props)}>
                 </div>
 
