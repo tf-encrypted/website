6d5ac712c9e4b32abb19e13c67320053a110417f
Ben DeCoste <bendecoste@gmail.com>
2019-06-27 15:33:44 -0300

    added tfe mailchimp url

--- pages/index.js
+++ pages/index.js
@@ -230,7 +230,7 @@ class Index extends React.Component {
                         <div className='small-block-content max'>
                             <h3>Subscribe to our mailing list</h3>
                     
-                        <form action='https://tf-encrypted.us3.list-manage.com/subscribe/post?u=9d309aaebe20f1ee0e4019019&amp;id=d411344ca8' method='post' id='mc-embedded-subscribe-form' name='mc-embedded-subscribe-form' className='validate' target='_blank' novalidate>
+                            <form action='https://tf-encrypted.us3.list-manage.com/subscribe/post?u=9d309aaebe20f1ee0e4019019&amp;id=d411344ca8' method='post' id='mc-embedded-subscribe-form' name='mc-embedded-subscribe-form' className='validate' target='_blank' novalidate>
                                 <div className='input-button-holder'>
                                     <input type='email' name='EMAIL' className='email' id='mce-EMAIL' placeholder='email address' required></input>
                                     <input className='button btn-secondary' value='Subscribe' type='submit'/>
